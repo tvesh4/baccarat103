bc9e0e1cb2bfc876266c12ea5a306a834b90bbc9
tvesh4 <86399970+tvesh4@users.noreply.github.com>
2023-04-19 23:15:31 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
 #include iostream
+#include <string>
+using namespace std;
+
+class Card
+{
+	private:
+		short int cardValue;
+		string suit;
+		short int face;
+
+	public:
+		//constructors
+		Card();
+		Card(short int, string);
+
+		//member functions
+		short int value();
+		short int getFace();
+		string getSuit();
