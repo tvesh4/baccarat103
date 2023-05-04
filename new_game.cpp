a9de1619591d561e2b82eae8ad4cd366fce9a1d5
tvesh4 <86399970+tvesh4@users.noreply.github.com>
2023-05-04 14:07:09 +0800

    Create new_game.cpp

---
+++ new_game.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "new_game.h"
+#include "detect_invalid_input.h"
+using namespace std;
+
+void new_game(double &initial, double &current, double total, int wins, double rate, string &file_name){
+    detect_invalid_input("Please set the initial money: ", "Invalid input.", current);
+    while (true){
+        if (current>=10 && current<=10000){
+            break;
+        }
+        else{
+            cout << "Range of initial money should be within $10 and $10,000" << endl;
+            detect_invalid_input("Please set the initial money: ", "Invalid input.", current);
+        }
+    }
+    initial = current; 
+    string name;
+    cout << "Enter a name for your new file: ";
+    cin >> name;
+    ofstream newgame;
+    newgame.open("gameFiles.txt", ios::app);
+    if (newgame.fail()){
+        cout << "Error in opening the file!" << endl;
+    }
+    newgame << name << endl;
+    newgame.close();
+    newgame.open(name.c_str());
+    if (newgame.fail()){
+        cout << "Error in opening the file!" << endl;
+    }
+    newgame << initial << " " << current << " " << total << " " << wins << " " << rate << endl;
+    newgame.close();
+    file_name = name;
+}
