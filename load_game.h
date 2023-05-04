3f9d1bbedc6696859d9d835eab20e2a5c521146f
dalight-luckyw <126152066+dalight-luckyw@users.noreply.github.com>
2023-05-04 14:02:33 +0800

    Added the code for the header file of load_game.cpp

--- load_game.h
+++ load_game.h
@@ -1,1 +1,8 @@
+#ifndef LOADGAME_H
+#define LOADGAME_H
 
+#include <string>
+
+void load_game(double &initial, double &current, double &total, int &wins, double &rate, std::string &file_name);
+
+#endif
