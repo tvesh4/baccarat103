66a2b8d1ceea762f6ee9ed0bb0b01d1a629f5411
KarmaYY22 <93028969+KarmaYY22@users.noreply.github.com>
2023-05-05 01:51:05 +0800

    Update save_game.cpp

--- save_game.cpp
+++ save_game.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "save_game.h"
+using namespace std;
 
+void save_game(double initial, double current, double total, int wins, double rate, string file_name){
+    ofstream save;
+    save.open(file_name.c_str());
+    if (save.fail()){
+        cout << "Error in opening the file!" << endl;
+    }
+    else{
+        save << initial << " " << current  << " " << total << " " << wins << " " << rate << endl;
+    }
+    cout << "Saving..." << endl;
+    cout << "******************" << endl;
+    cout << "Save Successful!" << endl;
+    cout << "******************" << endl;
+    save.close();
+}
