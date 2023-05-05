db8bdd56ebf8afbc6c3ff5ee44b722f89139e328
tvesh4 <86399970+tvesh4@users.noreply.github.com>
2023-05-05 15:13:55 +0800

    Added comments

--- Card.cpp
+++ Card.cpp
@@ -2,29 +2,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//class Card{
-//private:
-//    int cardValue;
-//    string suit;
-//    int faceValue;
-//public:
-//    Card();
-//    Card(int, string);
-//
-//    int getFace();
-//    int getValue();
-//    string getSuit();
-//
-//};
 
-Card::Card(){
+Card::Card(){ //The `Card` class has a default constructor that initializes the `cardValue` to 0, the `suit` to an empty string, and the `faceValue` to 0. 
     cardValue = 0;
     suit = "";
     faceValue = 0;
 }
 
 Card::Card(int value, string suit1){
-    faceValue = value;
+    faceValue = value; //Also has a parameterized constructor that sets the `faceValue` and `suit` based on the input values. 
     suit = suit1;
     if(value>9){
         cardValue = 0;
@@ -34,7 +20,8 @@ Card::Card(int value, string suit1){
     }
 }
 
-int Card::getValue(){
+//The `getValue()`, `getFace()`, and `getSuit()` member functions return the `cardValue`, `faceValue`, and `suit`, respectively.
+int Card::getValue(){ 
     return cardValue;
 }
 
@@ -46,44 +33,34 @@ string Card::getSuit(){
     return suit;
 }
 
-//class Hand{
-//private:
-//
-//public:
-//    Card card1, card2, card3;
-//    Hand();
-//    int getValue();
-//    void addCard(Card);
-//};
-
-Hand::Hand(){
+Hand::Hand(){ //The `Hand` class has a default constructor that initializes three `Card` objects with default `cardValue`, `suit`, and `faceValue`.
+    
+    //The `Card` class represents a playing card and has member functions to get the card's value, face value, and suit. 
     Card card1 = Card();
     Card card2 = Card();
     Card card3 = Card();
 }
 
-//Hand::Hand(Card card){
-//    Card card1 = card;
-//    Card card2 = Card();
-//    Card card3 = Card();
-//}
-Card Hand::getCard1() { return card1; }
+//The `getCard1()`, `getCard2()`, and `getCard3()` member functions return the first, second, and third `Card` objects in the hand, respectively. 
+Card Hand::getCard1() { return card1; } 
 Card Hand::getCard2() { return card2; }
 Card Hand::getCard3() { return card3; }
 
-void Hand::addCard(Card card){
-    if(card1.getFace()==0){
+//The `Hand` class represents a hand of cards and has member functions to get the cards in the hand, add a card to the hand, and calculate the value of the hand.
+
+void Hand::addCard(Card card){ //The `addCard()` member function adds a `Card` object to the hand. 
+    if(card1.getFace()==0){ //If the first `Card` object in the hand has a `faceValue` of 0, the new `Card` object is added to that position. 
         card1 = card;
     }
-    else if(card2.getFace()==0){
+    else if(card2.getFace()==0){ //Otherwise, if the second `Card` object in the hand has a `faceValue` of 0, the new `Card` object is added to that position. 
         card2 = card;
     }
     else{
-        card3 = card;
+        card3 = card; ////Otherwise, the new `Card` object is added to the third position. 
     }
 }
 
-int Hand::getValue(){
+int Hand::getValue(){ //The `getValue()` member function calculates and returns the total value of the hand, which is the sum of the `cardValue` of each `Card` object in the hand modulo 10.
     return (card1.getValue()+card2.getValue()+card3.getValue())%10;
 }
 
