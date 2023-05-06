ec273044a0dfbc18bd9da207f5cbdd639d6e573e
dalight-luckyw <126152066+dalight-luckyw@users.noreply.github.com>
2023-05-06 18:55:05 +0800

    Updated load_game.cpp to fit the use of array, released the memory of vector, appended ".txt" at the end of file_name, and added comments

--- load_game.cpp
+++ load_game.cpp
@@ -7,7 +7,7 @@
 #include "load_game.h"
 using namespace std;
 
-void load_game(double &initial, double &current, double &total, int &wins, double &rate, string &file_name){
+void load_game(double status[], string &file_name){
     ifstream load;
     load.open("gameFiles.txt");
     if (load.fail()){
@@ -16,7 +16,7 @@ void load_game(double &initial, double &current, double &total, int &wins, doubl
     double choice;
     int count=0;
     string name;
-    vector<string> names;
+    vector<string> names;  // Vector is used as it has a dynamic size to store the variable numbers of game files created by the user
     while(getline(load, name))
     {
         count++;
@@ -35,35 +35,37 @@ void load_game(double &initial, double &current, double &total, int &wins, doubl
         }
     }
     load.close();
-    names.clear();
+    names.clear(); // Size of the vector "names" is set to 0
+    vector<string>().swap(names); // Capacity of the vector "names" is set to 0 as well to release the memory completely.
+    file_name.append(".txt");
     string line, datum;
     load.open(file_name.c_str());
     if (load.fail()){
         cout << "Error in opening the file!" << endl;
     }
-    count = 1;
+    count = 0;
     while (getline(load, line)){
         istringstream data(line);
         while (data >> datum){
-            if (count == 1){
-                int int1 = stoi(datum); // convert the datum (string type) to int
-                initial = int1; // retrieve the initial money set by the user from the loaded file
+            if (count == 0){
+                int int0 = stoi(datum); // convert the datum (string type) to int
+                status[0] = int0; // retrieve the initial money set by the user from the loaded file
+            }
+            else if (count == 1){
+                int int1 = stoi(datum);
+                status[1] = int1; // retrieve the current balance from the loaded file
             }
             else if (count == 2){
                 int int2 = stoi(datum);
-                current = int2; // retrieve the current balance from the loaded file
+                status[2] = int2; // retrieve the total number of games played from the loaded file
             }
             else if (count == 3){
                 int int3 = stoi(datum);
-                total = int3; // retrieve the total number of games played from the loaded file
+                status[3] = int3; // retrieve the number of wins from the loaded file
             }
             else if (count == 4){
-                int int4 = stoi(datum);
-                wins = int4; // retrieve the number of wins from the loaded file
-            }
-            else if (count == 5){
-                double double5 = stod(datum);
-                rate = double5; // retrieve the winning rate from the loaded file
+                double double4 = stod(datum);
+                status[4] = double4; // retrieve the winning rate from the loaded file
             }
             count++;
         }
