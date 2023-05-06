bf7626c788bd4a0915fdcaba71c18719d36cdc9e
dalight-luckyw <126152066+dalight-luckyw@users.noreply.github.com>
2023-05-06 19:01:18 +0800

    Updated to fit the use of array

--- new_game.h
+++ new_game.h
@@ -3,6 +3,6 @@
 
 #include <string>
 
-void new_game(double &initial, double &current, double total, int wins, double rate, std::string &file_name);
+void new_game(double status[], std::string &file_name);
 
 #endif
