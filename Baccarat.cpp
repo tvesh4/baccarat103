58f347b4892bd644f32bc6f691582efae0f24dc2
dalight-luckyw <126152066+dalight-luckyw@users.noreply.github.com>
2023-05-06 18:35:46 +0800

    Added an array to store the data, made changes to fit the use of array

--- Baccarat.cpp
+++ Baccarat.cpp
@@ -27,7 +27,7 @@ void declareWinner(short, short, short);
 short checkWin(short, short, short);
 bool drawThird(short);
 bool bankDraw(short, short);
-void play(double &, double &, int &, double &);
+void play(double status[]);
 
 //Declares a constant named `LINE_LENGTH` with a value of 30 and a character variable named `LINE_SYMBOL` with a value of `*`.
 const int LINE_LENGTH = 30;
@@ -35,9 +35,15 @@ const char LINE_SYMBOL = '*';
 
 int main()
 {
-    double initial, money, total=0.0, rate=0;
+    double initial, money, total=0, wins=0, rate=0;
+    double game_status[5] = {initial, money, total, wins, rate};
+    // An array is created to store the data that determine the status of the game. The following illustrates the meaning of each index of the array:
+    // game_status[0] = initial money set by the user
+    // game_status[1] = balance of the user
+    // game_status[2] = total number of games played by the user
+    // game_status[3] = total number of games won by the user
+    // game_status[4] = the winning rate of the user
     double choice;
-    int wins=0;
     string file_name;
     ifstream file; //'ifstream' object is used to open a file. 
     file.open("gameFiles.txt");
@@ -55,11 +61,11 @@ int main()
     //continues running until the user selects a legitimate choice after the user's input is validated using the 'detect_invalid_input()' function.
     while (true){
         if (choice == 1){ //runs the 'new_game()' or 'load_game()' function as appropriate depending on whether the user wants to load their progress or begin a new game. 
-            new_game(initial, money, total, wins, rate, file_name);
+            new_game(game_status, file_name);
             break;
         } 
         else if ((choice == 2) &&!(file.fail())){
-            load_game(initial, money, total, wins, rate, file_name);
+            load_game(game_status, file_name);
             break;
         }
         else if ((choice == 2) && (file.fail())){
@@ -75,7 +81,7 @@ int main()
 
     while (true) { // 4 options are provided to the user.
         double choice;
-        showData(initial, total, rate);
+        showData(game_status[0], game_status[2], game_status[4]);
         cout << "Menu:" << std::endl;
         cout << "1. Start playing game" << std::endl;
         cout << "2. Check the balance" << std::endl;
@@ -84,15 +90,19 @@ int main()
         detect_invalid_input("Enter your choice (1, 2, 3, or 4): ", "Invalid choice. Please enter 1, 2, 3 or 4.", choice);
         if (choice == 1) { //To begin playing, the program calls the "play()" function after once more validating the user's input. 
             cout << "Starting the game..." << endl;
-            play(money, total, wins, rate);
-            if (money < 10){
+            play(game_status);
+            if (game_status[1] < 10){
                 cout << "You don't have enough money to bet anymore!" << endl;
                 cout << "Would you like to restart? (1. Yes/ 2. No and Quit)" << endl;
                 double decision;
                 detect_invalid_input("Enter your choice (1 or 2): ", "Invalid choice. Please enter 1 or 2.", decision);
                 while (true){
                     if (decision == 1){
-                        new_game(initial, money, total, wins, rate, file_name);
+                        // Some data are reset to 0 to allow the user to restart
+                        game_status[2] = 0;
+                        game_status[3] = 0;
+                        game_status[4] = 0;
+                        new_game(game_status, file_name);
                         break;
                     }
                     else if (decision == 2){ //shows the current balance if the user decides to check it. 
@@ -107,10 +117,10 @@ int main()
             }
         } else if (choice == 2) { //The application tells the user to cancel or resume the game if the user's balance drops below $10. 
             cout << "*************************" << endl;
-            cout << "You currently have $" << money << endl;
+            cout << "You currently have $" << game_status[1] << endl;
             cout << "*************************" << endl;
         } else if (choice == 3) {
-            save_game(initial, money, total, wins, rate, file_name); 
+            save_game(game_status, file_name); 
             //The'save_game()' function is used to save the game's progress if the user choose to save it. 
         } else if (choice == 4) {
             cout << "Exiting the game..." << endl;
@@ -122,7 +132,7 @@ int main()
     return 0; //Ends if the user decides to quit the game. The'main()' method concludes by returning 0.
 }
 
-void play(double &money, double &total, int &wins, double &rate){ //Called when the user chooses to start playing the game.
+void play(double status[]){ //Called when the user chooses to start playing the game.
     double choice; //Choose whether to bet on the player, banker, or tie using the `detect_invalid_input()` function to validate the input.
     detect_invalid_input("Please choose player or banker (1 for player, 2 for banker, 3 for tie): ", "Invalid choice. Please enter 1, 2 or 3.", choice);
     while (true){
@@ -138,14 +148,14 @@ void play(double &money, double &total, int &wins, double &rate){ //Called when 
     detect_invalid_input("How much money you want to play: $", "Invalid input.", playAmount);
     while(true){ 
         //Checks whether the user has enough money to make the bet, and if not, it displays an error message and prompts the user to enter a valid amount.
-        if(playAmount<=money && playAmount>=10){
+        if(playAmount<=status[1] && playAmount>=10){
             break; 
         }
         else if (playAmount<10){
             cout << "Sorry, the minimum bet to play Baccarat is $10." << endl;
         }
         else{
-            cout << "Sorry, not enough balance. (Your current balance: $" << money << ")" << endl;
+            cout << "Sorry, not enough balance. (Your current balance: $" << status[1] << ")" << endl;
         }
         detect_invalid_input("How much money you want to play: $", "Invalid input.", playAmount);
     }
@@ -221,22 +231,22 @@ void play(double &money, double &total, int &wins, double &rate){ //Called when 
     }
     
     declareWinner(winner, player.getValue(), bank.getValue());
-    total++; //If the user wins the bet, the function adds the payout to their balance. 
+    status[2]++; //If the user wins the bet, the function adds the payout to their balance. 
     if(winner == 3 && choice == winner){ 
-        money += playAmount*8; // Tie payout 8:1
-        wins++;
+        status[1] += playAmount*8; // Tie payout 8:1
+        status[3]++;
     }
     else if(choice == winner){
-        money += playAmount; // Player or Banker payout 1:1
-        wins++;
+        status[1] += playAmount; // Player or Banker payout 1:1
+        status[3]++;
     }
     else if(winner == 3){
         //If there is a tie, the function returns the bet amount to those who bet on the player or the banker.
     }
     else{
-        money -= playAmount; // If the user loses the bet, declareWinner() subtracts the bet amount from their balance.
+        status[1] -= playAmount; // If the user loses the bet, declareWinner() subtracts the bet amount from their balance.
     }
-    rate = (wins/total)*100;
+    status[4] = (status[3]/status[2])*100;
 }
 
 void declareWinner(short winner, short player, short bank) 
