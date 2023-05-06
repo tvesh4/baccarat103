fd5ee29e518e4a42f16fc59d54d8fc2236b101d9
dalight-luckyw <126152066+dalight-luckyw@users.noreply.github.com>
2023-05-06 19:04:40 +0800

    Updated to fit the use of array

--- save_game.cpp
+++ save_game.cpp
@@ -4,14 +4,17 @@
 #include "save_game.h"
 using namespace std;
 
-void save_game(double initial, double current, double total, int wins, double rate, string file_name){
+void save_game(double status[], string file_name){
     ofstream save;
     save.open(file_name.c_str());
     if (save.fail()){
         cout << "Error in opening the file!" << endl;
     }
     else{
-        save << initial << " " << current  << " " << total << " " << wins << " " << rate << endl;
+        for (int i=0; i<5; i++){
+            save << status[i] << " ";
+        }
+        save << endl;
     }
     cout << "Saving..." << endl;
     cout << "******************" << endl;
