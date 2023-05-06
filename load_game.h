4f3ca12d0b74aa6cbf075f4eaf39531ef3738113
dalight-luckyw <126152066+dalight-luckyw@users.noreply.github.com>
2023-05-06 18:41:48 +0800

    Removed several input arguments and added an array as argument

--- load_game.h
+++ load_game.h
@@ -3,6 +3,6 @@
 
 #include <string>
 
-void load_game(double &initial, double &current, double &total, int &wins, double &rate, std::string &file_name);
+void load_game(double status[], std::string &file_name);
 
 #endif
